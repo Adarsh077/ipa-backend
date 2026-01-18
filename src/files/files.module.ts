1a9ce3d4e2672a2026a6c23d53e1906ed49e6193
Adarsh Senghani <adarshco077@gmail.com>
2026-01-18 21:49:31 +0530

    feat(files): removed files controller

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 import { FilesResolver } from './files.resolver';
 
 @Module({
-  controllers: [FilesController],
   providers: [FilesService, FilesResolver],
 })
 export class FilesModule {}
